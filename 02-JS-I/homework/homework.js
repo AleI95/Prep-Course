2c9ec209fcbc53d58b4c46389a8e261da4546382
Alejandro <androlp1995@gmail.com>
2022-08-07 17:12:59 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 19;
+const nuevoNum = 15;
 
 // Crea una variable booleana:
 const nuevoBool = true;
